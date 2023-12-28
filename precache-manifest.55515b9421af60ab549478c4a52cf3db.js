d8d05e4636331d7c3a552c8ec0f8386327b312bb
long <tranlong8102000@gmail.com>
2023-12-29 00:53:54 +0700

    Updates

---
+++ precache-manifest.55515b9421af60ab549478c4a52cf3db.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d7cb7164f0fe20129251efecd38e5f2",
+    "url": "/ecommerce-client/index.html"
+  },
+  {
+    "revision": "3e71133942d2741bd0af",
+    "url": "/ecommerce-client/static/css/2.79df26c8.chunk.css"
+  },
+  {
+    "revision": "6eb32c697aa02fb56422",
+    "url": "/ecommerce-client/static/css/main.8b93b7df.chunk.css"
+  },
+  {
+    "revision": "3e71133942d2741bd0af",
+    "url": "/ecommerce-client/static/js/2.7cd8feb7.chunk.js"
+  },
+  {
+    "revision": "6eb32c697aa02fb56422",
+    "url": "/ecommerce-client/static/js/main.505e446a.chunk.js"
+  },
+  {
+    "revision": "cc08fa1b06f85e84c9d6",
+    "url": "/ecommerce-client/static/js/runtime-main.01786dd4.js"
+  },
+  {
+    "revision": "51f74cb7f4ee4d652d9fe671e919be6c",
+    "url": "/ecommerce-client/static/media/ip-01.51f74cb7.jpg"
+  },
+  {
+    "revision": "cc7990286aecdbf7fb3fe21a785a63fa",
+    "url": "/ecommerce-client/static/media/ipad1-01.cc799028.jpg"
+  },
+  {
+    "revision": "7d26dd9f3cce1822757a63b064abf842",
+    "url": "/ecommerce-client/static/media/laptop-01.7d26dd9f.jpg"
+  },
+  {
+    "revision": "f7ee2d33cc517e260c4d313590909332",
+    "url": "/ecommerce-client/static/media/mb-01.f7ee2d33.jpg"
+  },
+  {
+    "revision": "2518f2481c9aa7273fa0561afc83f2ef",
+    "url": "/ecommerce-client/static/media/msi-01.2518f248.jpg"
+  },
+  {
+    "revision": "845a1bf1b15f6e2232140d1e072133e1",
+    "url": "/ecommerce-client/static/media/phone 2-01.845a1bf1.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ecommerce-client/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ecommerce-client/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ecommerce-client/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ecommerce-client/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b386fdba33fe3a3213e1e50a3fb244ee",
+    "url": "/ecommerce-client/static/media/tablet 2-01.b386fdba.jpg"
+  }
+]);
